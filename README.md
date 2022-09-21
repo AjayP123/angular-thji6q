ab3fbebb13a5ff7452bda04872b16a9b8eb3d0fc
AjayP123 <30439725+AjayP123@users.noreply.github.com>
2022-09-21 14:54:41 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-thji6q
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-thji6q)
---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MaterialExampleModule } from '../material.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatNativeDateModule } from '@angular/material/core';
+import { HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from './home/home.component';
+import { ScrollTopComponent } from './scroll-top/scroll-top.component';
+
+@NgModule({
+  declarations: [HomeComponent, ScrollTopComponent],
+  imports: [
+    BrowserAnimationsModule,
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    MatNativeDateModule,
+    MaterialExampleModule,
+    ReactiveFormsModule,
+  ],
+  providers: [],
+  bootstrap: [HomeComponent],
+})
+export class AppModule {}
